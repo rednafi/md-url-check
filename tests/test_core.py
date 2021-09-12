12b1d4e1abca0d4eb03642444ab1b7ed7ccf075e
Redowan Delowar <redowan.nafi@gmail.com>
2021-09-12 20:18:30 +0600

    Added test for verify_links routine

--- tests/test_core.py
+++ tests/test_core.py
@@ -8,8 +8,10 @@ def test_color(capsys):
     color = core.Color
 
     s = f"{color.YELLOW}hello{color.RESET}"
+
     print(s)
     out, err = capsys.readouterr()
+
     assert err == ""
     assert "\x1b[93mhello\x1b[0m\n" in out
 
@@ -25,16 +27,20 @@ def test_read_chunk(tmp_path, capsys):
     Content line 3
     Content line 4
     """
+
     p.write_text(md_content)
-    out, err = capsys.readouterr()
 
     chunks = core._read_chunk(file_path=str(p), line_count=2)
+
     for chunk in chunks:
         assert len(chunk.strip().split("\n")) == 1 or 2
+        print(chunk)
 
-    "" in err
-    "Content line 1" in out
-    "Content line 2" in out
+    out, err = capsys.readouterr()
+
+    assert err == ""
+    assert "Content line 1" in out
+    assert "Conetent line 2" in out
 
 
 def test_find_links_from_chunk():
@@ -51,7 +57,7 @@ def test_find_links_from_chunk():
     ]
 
 
-def test_find_links_from_markdown(tmp_path, capsys, markdown_text):
+def test_find_links_from_markdown(tmp_path, markdown_text):
     d = tmp_path / "dest"
     d.mkdir()
     p = d / "file.md"
@@ -81,10 +87,24 @@ def test_log_request(capsys):
         core._log_request(url="https://httpbin.org/get")
 
     out, err = capsys.readouterr()
-    assert "" in err
+    assert err == ""
     assert (
         """\n\x1b[1m\x1b[95mURL\x1b[0m: \x1b[92mhttps://httpbin.org/get\x1b[0m\n\x1b[1m\x1b[93m"""
         in out
     )
     assert "Status Code\x1b[0m:  \x1b[94m200\x1b[0m ✅\n\x1b[1m\x1b[96m" in out
     assert "Status Description\x1b[0m: Request fulfilled, document follows\n\n" in out
+
+
+def test_verify_links(tmp_path, capsys, markdown_text):
+    d = tmp_path / "dest"
+    d.mkdir()
+    p = d / "file.md"
+    p.write_text(markdown_text)
+
+    with patch("md_url_check.core._make_request", lambda url: 200):
+        core.verify_links(markdown_path=str(p))
+    out, err = capsys.readouterr()
+
+    assert err == ""
+    assert "\x1b[92mAll checks succeeded\x1b[0m 🎉\n" in out
